0a4a16c73e4aee327143740404128c7b01f5c2a1
Daniel <fiend.of.sound@gmail.com>
2022-12-31 02:30:22 +0400

    feat: stable party integration

--- src/db/party.ts
+++ src/db/party.ts
@@ -7,9 +7,18 @@ export const pipeInstantPartyData = async (request, env) => {
   const u = new URL(request.url)
   const partyId = u.searchParams.get("partyId")
   const sql = `
-	  select * from (select * from _default as party join _default as msg 
-	  on party.id = msg.chatId where msg.messageType in ['party_media'] 
-	  and party.partyId = $1 order by msg.createdAt desc limit 1) as partyMedia
+	  ( 
+	  	select * from _default as sync
+	  	where messageType = 'party_media_sync'
+	  	and chatId = $1 
+	  	order by createdAt desc limit 1
+	  )
+	  UNION
+  	(
+	  	select * from _default as party join _default as media 
+		  on party.id = media.chatId where media.messageType = 'party_media'
+		  and party.partyId = $1 order by media.createdAt desc limit 1
+	  )
   `
   const args = [partyId]
   return pipeDBRequest(env, sql, args)
